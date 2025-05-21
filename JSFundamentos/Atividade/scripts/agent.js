aa3355641fc3e669a70f1e42869a2012b5b8b669
heloisa <heloisa.dleal@gmail.com>
2025-05-21 09:01:21 -0300

    lab0

--- JSFundamentos/Atividade/scripts/agent.js
+++ JSFundamentos/Atividade/scripts/agent.js
@@ -1,3 +1,36 @@
+//const data_agents = []
 
-const data_agents = []
+let id_agent = 1
 
+function addAgent(objRegional) {
+
+    const form = document.querySelector('#formAgent')
+    const input_agent = form.querySelector('input[name="nome"]')
+    const table = document.querySelector('#table_agent')
+
+    const line = document.createElement('tr')
+    const col_id = document.createElement('td')
+    const col_nome = document.createElement('td')
+
+    col_id.textContent = id_agent
+    id_agent++
+    col_nome.textContent = input_agent.value
+
+    line.appendChild(col_id)
+    line.appendChild(col_nome)
+
+    table.appendChild(line)
+
+
+    const select = document.querySelector('#select_regional');
+    const op = document.createElement("option");
+         
+    op.textContent = objRegional_sigla.value
+    op.value = objRegional_sigla.value
+    select.appendChild(op)
+
+    form.reset()
+
+}
+
+//erro no obj
--- JSFundamentos/Atividade/scripts/regional.js
+++ JSFundamentos/Atividade/scripts/regional.js
@@ -1,26 +1,33 @@
 //const data_regionals = []
 
-let id = 1;
+let id_regional = 1;
 function addRegional(){
 
     const form = document.querySelector('#formRegional')
-    const input_descricao = form.querySelector('input[name="regional"]')
+    const input_sigla = form.querySelector('input[name="sigla"]')
+    const input_cidade = form.querySelector('input[name="cidade"]')
     const table = document.querySelector('#table_regional');
  
     const line = document.createElement('tr')
     const col_id = document.createElement('td')
     const col_sigla = document.createElement('td')
-    const col_city = document.createElement('td')
+    const col_cidade = document.createElement('td')
 
-    col_id.textContent = id; 
-    id++;
-    col_sigla.textContent = input_descricao.value
-    col_city.textContent = input_descricao.value
+    col_id.textContent = id_regional; 
+    id_regional++;
+    col_sigla.textContent = input_sigla.value
+    col_cidade.textContent = input_cidade.value
+
+    const objRegional = {sigla: input_sigla.value, cidade: input_cidade.value}
+    addSelectRegional(objRegional);
 
     line.appendChild(col_id);
     line.appendChild(col_sigla);
-    line.appendChild(col_city);
+    line.appendChild(col_cidade);
  
     table.appendChild(line);
+    
 }
+
+    
 
