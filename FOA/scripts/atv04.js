93d9a21f3d1e6e4e3f553be912dc484a9741e3a3
chrissnts <christiandossantos125@gmail.com>
2025-06-02 18:14:46 -0300

    lab4

--- FOA/scripts/atv04.js
+++ FOA/scripts/atv04.js
@@ -3,15 +3,25 @@ function ex04() {
     
     const form = document.querySelector('#form04')
     const input = form.querySelector('input[name="in_04"]').value
-    alert(input)
+
+    const num = input.split(" ").map(Number)
+    let perfeito = resolve04(...num);
+
     form.reset()
+    document.getElementById('output').innerText = perfeito.join(" ");
 }
 
-function resolve04() {
-
+function resolve04(...numeros) {
+    return numeros.filter(filter_perfeito);
 }
 
-const filter_perfeito = (val) => {
-
+function filter_perfeito(val){
+    let soma = 0
+    for (let i=1; i<val; i++){
+        if(val % i == 0){
+            soma += i;
+        }
+    }
+    return soma == val;
 }
 
--- FOA/scripts/atv05.js
+++ FOA/scripts/atv05.js
@@ -3,14 +3,21 @@ function ex05() {
     
     const form = document.querySelector('#form05')
     const input = form.querySelector('input[name="in_05"]').value
-    alert(input)
+    
+    let obj;
+    obj = JSON.parse(input); // Converte string JSON em objeto
+   
+    let novaSaída = resolve05(obj);
+
     form.reset()
+    document.getElementById('output').innerText = `${novaSaída}`;
 }
 
 function resolve05(obj) {
-
+    return construtora(obj);
 }
 
 function construtora(data) {
-
+    return `Id: ${data.id}, Nome: ${data.nome}, Média: ${data.media}`;
 }
+
